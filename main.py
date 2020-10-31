4c05d1cb2050b4ac8371348bdbe2e9bcb7ef65ec
Kostya Khushensky <46225922+KonstDev@users.noreply.github.com>
2020-10-31 18:53:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,56 @@
-import HLTV
-import vk_api
 import telebot
-from vk import utils
+import pyshorteners as pysh
+import requests
 
-bot = telebot.TeleBot('1319312903:AAH4x6ERJFenG1itR5UexZfB8tBUHuccuvw')
-#print(HLTV.get_upcoming_matches())
+
+
+
+
+
+
+#text1 = "hello"
+# print(HLTV.get_upcoming_matches())
+
+bot = telebot.TeleBot('1396209391:AAFJE_mGt8VmtBu0npi__qAn-66DsGcHguA')
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAANfX3y1pDJrr3kE7WrpnNXAWRnljtwAAuQDAAKJ6uUH96krUY45jZ8bBA')
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAANfX3y1pDJrr3kE7WrpnNXAWRnljtwAAuQDAAKJ6uUH96krUY45jZ8bBA')
+
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    bot.send_message(utils.getShortLink(str(message.text), "fe4e5280fe4e5280fe4e528084fe3a0871ffe4efe4e5280a1cd4c4f33114dce4b289b95"))
+    #Authorization =
+    #url = "https://api-ssl.bitly.com/v4/shorten"
+    #values = {'url': message}
+    #r = requests.post(url, params=values)
+   # resp = r.text
+    url = message.text
+    resp = pysh.Shortener().tinyurl.short(url)
+    print(message.text, " : ", resp)
+    bot.send_message(message.chat.id,resp)
+
+
 #   if message.text.lower() == 'live':
- #      bot.send_message(message.chat.id, HLTV.get_live_matches())
-  # elif message.text.lower() == 'upcoming':
-   #    bot.send_message(message.chat.id, HLTV.get_upcoming_matches())
-   #elif message.text.lower() == 'ты лох':
-    #   bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMQX3tVfMBvgokE-8TJzjwY8rMVSbUAAlQAAy7yXCUakl03HYs5qhsE')
-   #elif message.text.lower() == 'mylogin':
-    #   bot.send_message(message.chat.id, message.chat.username)
-  # elif message.text == 'ямыбатька':
-   #    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMmX3tdiPad77cbjiAPKbst_dVqnncAAu0AA6tXxAsI817NM7dIhBsE')
-    #   #CAACAgIAAxkBAAMQX3tVfMBvgokE-8TJzjwY8rMVSbUAAlQAAy7yXCUakl03HYs5qhsE
+#      bot.send_message(message.chat.id, HLTV.get_live_matches())
+# elif message.text.lower() == 'upcoming':
+#    bot.send_message(message.chat.id, HLTV.get_upcoming_matches())
+
+# elif message.text.lower() == 'ты лох':
+
+#   bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMQX3tVfMBvgokE-8TJzjwY8rMVSbUAAlQAAy7yXCUakl03HYs5qhsE')
+# elif message.text.lower() == 'mylogin':
+#   bot.send_message(message.chat.id, message.chat.username)
+# elif message.text == 'ямыбатька':
+#    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMmX3tdiPad77cbjiAPKbst_dVqnncAAu0AA6tXxAsI817NM7dIhBsE')
+#   #CAACAgIAAxkBAAMQX3tVfMBvgokE-8TJzjwY8rMVSbUAAlQAAy7yXCUakl03HYs5qhsE
 @bot.message_handler(content_types=['sticker'])
 def send_sticker(message):
     print(message.sticker.file_id)
-#.file_id
-#GRAY
-#GUKAY
-#VYASNY ZYALYLAY TEPLAY CHARY
+
+
+# .file_id
+# GRAY
+# GUKAY
+# VYASNY ZYALYLAY TEPLAY CHARY
 
 bot.polling()

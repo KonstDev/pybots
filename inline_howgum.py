6514f5d2c4c37bcf5a244d6ba2b390ac7d678386
Kostya Khushensky <46225922+KonstDev@users.noreply.github.com>
2020-12-17 18:36:38 +0300

    Add files via upload
    Тow the percentage depends on the day of the week

--- inline_howgum.py
+++ inline_howgum.py
@@ -1,19 +1,14 @@
 import telebot
 import random
 from telebot import types
-import time
+import datetime
 
-bot = telebot.TeleBot('')
+bot = telebot.TeleBot('1457557479:AAELLNrkOVaurYh5rQh9bnPN95-UTvnMe70')
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, напиши мне /help')
-
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=['uid'])
 def send_text(message):
-    bot.send_message(message.chat.id, 'Привет, вот мои команды: ')
-    bot.send_message(message.chat.id, 'команда /sc определяет насколько вы технарь')
+    bot.send_message(message.chat.id, message.from_user.id)
 
 
 
@@ -23,12 +18,13 @@ def query_text(query):
     kb = types.InlineKeyboardMarkup()
     kb.add(types.InlineKeyboardButton(text="", callback_data="test"))
     results = []
+    random.seed(query.from_user.id + int(datetime.datetime.today().isoweekday()))
     cache_time = 0
-    guum = random.randint(0,4)
+    guum = random.randint(0,6)
     rrr = random.randrange(1, 101, 1)
-    if (guum == 0 or guum == 1):
+    if (guum == 0 or guum == 1 or guum == 2):
         rrrstr = 'Я технарь на ' + str(rrr) + '%!'
-    elif (guum == 2 or guum == 3):
+    elif (guum == 4 or guum == 3 or guum == 5):
         rrrstr = 'Я гуманитарий на ' + str(rrr) + '%!'
     else:
         rrrstr = 'Tyanochku by'
